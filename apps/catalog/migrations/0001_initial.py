10607c8195b730057b2f84ff4489e5c1e894d861
Nirvik Karki <karkinirvik@gmail.com>
2025-11-20 16:31:45 +0545

    Catalog, Order, Coupon, Checkout

--- apps/catalog/migrations/0001_initial.py
+++ apps/catalog/migrations/0001_initial.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
                 ('price', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('image', cloudinary.models.CloudinaryField(max_length=255, verbose_name='image')),
                 ('description', models.TextField(blank=True)),
-                ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='ecommerce.productcategory')),
+                ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='catalog.productcategory')),
             ],
         ),
     ]
