6c94a47140502e6f767f77af669b3425a38024c2
Nirvik Karki <karkinirvik@gmail.com>
2025-12-22 16:30:40 +0545

    New models in Project

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -2,7 +2,22 @@ from rest_framework.routers import DefaultRouter
 from .views import ServiceViewSet, ProjectViewSet
 
 router = DefaultRouter()
-router.register("services", ServiceViewSet)
-router.register("projects", ProjectViewSet)
+from .views import (
+    SectorViewSet,
+    ServiceViewSet,
+    ServiceListViewSet,
+    ProjectViewSet,
+    ProjectGalleryImageViewSet,
+)
+
+router.register(r"sectors", SectorViewSet, basename="sector")
+router.register(r"services", ServiceViewSet, basename="service")
+router.register(r"service-lists", ServiceListViewSet, basename="service-list")
+router.register(r"projects", ProjectViewSet, basename="project")
+router.register(
+    r"project-gallery-images",
+    ProjectGalleryImageViewSet,
+    basename="project-gallery-image"
+)
 
 urlpatterns = router.urls
